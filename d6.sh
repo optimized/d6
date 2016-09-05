a782ef5d73d4c80b1475df1fb2c80b0068685311
Noël Jackson <n@noeljackson.com>
2016-09-04 20:06:01 -0400

    backup command that needs to have an argument

--- d6.sh
+++ d6.sh
@@ -13,6 +13,9 @@ d6(){
 	shellcontainer)
 		docker exec -it noeljacksoncom_wordpress_1 bash
 		;;
+	backup)
+		docker run --rm   --volumes-from ddc1a7eae4f1   -v $(pwd):/backup  boombatower/docker-backup
+		;;
 
       *)
             echo $"Usage: $0 {start|stop|restart|condrestart|status}"
